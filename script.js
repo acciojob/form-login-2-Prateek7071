f261a38cfcc485bbdaf03bb5625ff2431afc6bea
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-12 17:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const Fname=document.getElementById('Fname')
+const Lname=document.getElementById('Lname')
+const Pnumber=document.getElementById('Pnumber')
+const Email=document.getElementById('Email')
+const Submit=document.getElementById('Submit')
+
+Submit.addEventListener('click',(e)={
+	e.preventDefault()
+	Alert(`First Name: ${Fname} Last Name: ${Lname} Phone Number: ${Pnumber} Email ID: ${Email}`)
+})
